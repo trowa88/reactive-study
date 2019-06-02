b054dbabda07233953561e3fdd76ef50ed88850d
colin <trowa020@gmail.com>
2019-06-02 16:20:18 +0900

    Observer 간단 동작

--- src/main/java/com/colin/reactive/observer/Ob.java
+++ src/main/java/com/colin/reactive/observer/Ob.java
@@ -3,6 +3,8 @@ package com.colin.reactive.observer;
 import java.util.Iterator;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Ob {
     //    public static void main(String[] args) {
@@ -40,13 +42,17 @@ public class Ob {
         Observer observer = new Observer() {
             @Override
             public void update(Observable o, Object arg) {
-                System.out.println(arg);
+                System.out.println(Thread.currentThread().getName() + " " + arg);
             }
         };
 
         IntObservable intObservable = new IntObservable();
         intObservable.addObserver(observer);
 
-        intObservable.run();
+        ExecutorService es = Executors.newSingleThreadExecutor();
+        es.execute(intObservable);
+
+        System.out.println(Thread.currentThread().getName() + " EXIT");
+        es.shutdown();
     }
 }
