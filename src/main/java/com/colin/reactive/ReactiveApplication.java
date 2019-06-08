b533c37e47b388822441a6e26a03cae52be3a389
colin <trowa020@gmail.com>
2019-06-08 17:36:07 +0900

    Reactor 예제 1.

--- src/main/java/com/colin/reactive/ReactiveApplication.java
+++ src/main/java/com/colin/reactive/ReactiveApplication.java
@@ -1,10 +1,32 @@
 package com.colin.reactive;
 
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscription;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ReactiveApplication {
+    @RestController
+    public static class Controller {
+        @RequestMapping("/hello")
+        public Publisher<String> hello(String name) {
+            return s -> s.onSubscribe(new Subscription() {
+                @Override
+                public void request(long n) {
+                    s.onNext("Hello " + name);
+                    s.onComplete();
+                }
+
+                @Override
+                public void cancel() {
+
+                }
+            });
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ReactiveApplication.class, args);
---
+++ src/main/java/com/colin/reactive/example/ReactorEx.java
@@ -0,0 +1,19 @@
+package com.colin.reactive.example;
+
+import reactor.core.publisher.Flux;
+
+public class ReactorEx {
+    public static void main(String[] args) {
+        Flux.<Integer>create(e -> {
+            e.next(1);
+            e.next(2);
+            e.next(3);
+            e.complete();
+        })
+        .log()
+        .map(s -> s * 10)
+        .reduce(0, Integer::sum)
+        .log()
+        .subscribe(System.out::println);
+    }
+}
